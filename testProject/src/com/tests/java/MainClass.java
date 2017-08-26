cee85e3a7d5fa6dbf9d1462c2b311f9ffee9823c
FabioLee88 <fabiolee88@hotmail.it>
2017-08-26 18:18:30 +0200

    addedd test java project on usb

---
+++ testProject/src/com/tests/java/MainClass.java
@@ -0,0 +1,10 @@
+package com.tests.java;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		TestClass c = new TestClass();
+		c.setNome("Prova il nome");
+		System.out.println(c.getNome());
+	}
+}
